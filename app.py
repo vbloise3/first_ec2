7db37e6e6cc43fbde832f0316708b1c0cf64f15d
Vince Bloise <vbloise3@gmail.com>
2019-12-30 18:59:49 -0500

    got the full vpc and ec2 create cdk working

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from aws_cdk import core
 from first_ec2.first_ec2_stack import FirstEc2Stack
 
 
+#app = core.App()
+#FirstEc2Stack(app, "first-ec2")
+
 app = core.App()
-FirstEc2Stack(app, "first-ec2")
+FirstEc2Stack(app, "first-ec2", env=core.Environment(region="us-east-1", account="001178231653"))
 
 app.synth()
--- first_ec2/first_ec2_stack.py
+++ first_ec2/first_ec2_stack.py
@@ -1,4 +1,9 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_autoscaling as autoscaling,
+    aws_ec2 as ec2,
+    aws_elasticloadbalancing as elb,
+    core
+)
 
 
 class FirstEc2Stack(core.Stack):
@@ -7,3 +12,51 @@ class FirstEc2Stack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(
+            self, "MyVpc",
+            max_azs=2
+        )
+
+        sg = ec2.SecurityGroup(
+            self, "SG",
+            description='Allow ssh access to ec2 instances',
+            vpc=vpc
+        )
+
+        sg.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(22)
+        )
+
+        asg = autoscaling.AutoScalingGroup(
+            self, "ASG",
+            vpc=vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
+            ),
+            machine_image=ec2.AmazonLinuxImage(),
+            desired_capacity=3,
+        )
+
+        lb = elb.LoadBalancer(
+            self, "LB",
+            vpc=vpc,
+            internet_facing=True,
+            health_check={"port": 80}
+        )
+        lb.add_target(asg)
+
+        ec2instance = ec2.Instance(
+            self, "EC2INSTANCE",
+            vpc=vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
+            ),
+            machine_image=ec2.AmazonLinuxImage(),
+            vpc_subnets={ 'subnet_type': ec2.SubnetType.PUBLIC },
+            security_group=sg,
+            key_name="MyNVKeyPair"
+        )
+
+        listener = lb.add_listener(external_port=80)
+        listener.connections.allow_default_port_from_any_ipv4("Open to the world")
