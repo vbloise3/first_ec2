fd8a015448e2f587a9c8e8a3303e562e941b4477
Vince Bloise <vbloise3@gmail.com>
2020-01-01 17:22:15 -0500

    completed ec2 lab

--- first_ec2/first_ec2_stack.py
+++ first_ec2/first_ec2_stack.py
@@ -1,10 +1,7 @@
 from aws_cdk import (
-    aws_autoscaling as autoscaling,
     aws_ec2 as ec2,
-    aws_elasticloadbalancing as elb,
     core
 )
-
 
 class FirstEc2Stack(core.Stack):
 
@@ -14,7 +11,7 @@ class FirstEc2Stack(core.Stack):
         # The code that defines your stack goes here
         vpc = ec2.Vpc(
             self, "MyVpc",
-            max_azs=2
+            max_azs=1
         )
 
         sg = ec2.SecurityGroup(
@@ -28,24 +25,6 @@ class FirstEc2Stack(core.Stack):
             connection=ec2.Port.tcp(22)
         )
 
-        asg = autoscaling.AutoScalingGroup(
-            self, "ASG",
-            vpc=vpc,
-            instance_type=ec2.InstanceType.of(
-                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
-            ),
-            machine_image=ec2.AmazonLinuxImage(),
-            desired_capacity=3,
-        )
-
-        lb = elb.LoadBalancer(
-            self, "LB",
-            vpc=vpc,
-            internet_facing=True,
-            health_check={"port": 80}
-        )
-        lb.add_target(asg)
-
         ec2instance = ec2.Instance(
             self, "EC2INSTANCE",
             vpc=vpc,
@@ -53,10 +32,7 @@ class FirstEc2Stack(core.Stack):
                 ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
             ),
             machine_image=ec2.AmazonLinuxImage(),
-            vpc_subnets={ 'subnet_type': ec2.SubnetType.PUBLIC },
+            vpc_subnets={'subnet_type': ec2.SubnetType.PUBLIC},
             security_group=sg,
             key_name="MyNVKeyPair"
         )
-
-        listener = lb.add_listener(external_port=80)
-        listener.connections.allow_default_port_from_any_ipv4("Open to the world")
